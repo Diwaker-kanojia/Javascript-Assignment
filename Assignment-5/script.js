33ce7ca7cbb08c1572785cd9e6a9b7f7f0944695
Diwaker-kanojia <diwakerkanojia17@gmail.com>
2025-09-07 21:52:32 +0530

    Update script.js

--- Assignment-5/script.js
+++ Assignment-5/script.js
@@ -52,3 +52,19 @@ let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2];
 let arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26];
 let sum = sumOfTwoArray(arr_1, arr_2);
 console.log(sum);
+
+//? Q5.Using a for loop output the elements in reverse order
+function reverseArray(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = temp;
+  }
+  return arr;
+}
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+console.log("----- Before Reversing -----");
+console.log(arr);
+reverseArray(arr);
+console.log("----- After Reversing -----");
+console.log(arr);
