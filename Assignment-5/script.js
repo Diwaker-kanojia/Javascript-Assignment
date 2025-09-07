aad4d4aa741379470d0014e0649222782603725f
Diwaker-kanojia <diwakerkanojia17@gmail.com>
2025-09-07 21:48:24 +0530

    Update script.js

--- Assignment-5/script.js
+++ Assignment-5/script.js
@@ -1,1 +1,11 @@
-
+//? Q1. Write a JavaScript program to construct the following pattern using a nested for loop.
+function printPattern(row) {
+  for (let i = 1; i <= row; i++) {
+    let string = "";
+    for (let j = 1; j <= i; j++) {
+      string += "* ";
+    }
+    console.log(string);
+  }
+}
+printPattern(5);
